674b11d4240fea25d29811f4498e0ab58b0b568b
Jason P. Pickering <jason.p.pickering@gmail.com>
2021-03-23 10:24:01 +0100

    Remove deprecated code

--- R/utils.R
+++ R/utils.R
@@ -70,49 +70,16 @@ validatePSNUData <- function(d,d2_session) {
                                                  vr = vr_rules,
                                                  d2session = d2_session)
   
-  # rules_to_keep <- c(
-  #   "L76D9NGEPRS",
-  #   "rVVZmdG1KTb",
-  #   "zEOFo6X436M",
-  #   "oVtpQHVVeCV",
-  #   "r0CC6MQW5zc",
-  #   "vrS3kAtlJ4F",
-  #   "WB338HNucS7",
-  #   "tiagZGzSh6G",
-  #   "vkFHYHgfqCf",
-  #   "coODsuNsoXu",
-  #   "qOnTyseQXv8",
-  #   "Ry93Kc34Zwg",
-  #   "g0XwMGLB5XP",
-  #   "eb02xBNx7bD",
-  #   "SNzoIyNuanF",
-  #   "Ry93Kc34Zwg",
-  #   "WiRJutVpAq4"
-  # )
-  
-  rules_to_ignore<-c("RLXOqAeHN04","KdqWm8ZvWoO")
-  
-  if (d$info$cop_year == 2020) {
-    rules_to_ignore<-c(rules_to_ignore,"JoKXq9knuI6","ApCcAi965Ud")
-  }
-  
   vr_violations<-vr_violations[!(vr_violations$id %in% rules_to_ignore),]
   
   if ( NROW(vr_violations) > 0 ) {
     
-    # vr_violations <-
-    #   vr_violations[vr_violations$id %in% rules_to_keep,] } else {
-    #     d$datim$vr_rules_check <- NULL
-    #     return(d)
-    #   }
-    
+
     diff <- gsub(" [<>]= ", "/", vr_violations$formula)
     vr_violations$diff <- sapply( diff, function(x) { round( ( eval( parse( text = x ) ) - 1) * 100, 2 ) })
     vr_violations$diff <-ifelse(vr_violations$rightSide.expression == 0 | vr_violations$leftSide.expression == 0,
                                 NA,
                                 vr_violations$diff)
-    
-    #vr_violations %<>% dplyr::filter(diff >= 5)
     
     diff <- gsub(" [<>]= ", "-", vr_violations$formula)
     vr_violations$abs_diff <- sapply( diff, function(x) { abs( eval( parse( text = x ) ) ) })
