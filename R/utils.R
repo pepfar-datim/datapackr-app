5a6e09043affbb4735d3bd287e755a6dd542aa2d
Jason P. Pickering <jason.p.pickering@gmail.com>
2021-04-06 09:51:24 +0200

    Allow for deault mechanism

--- R/utils.R
+++ R/utils.R
@@ -144,6 +144,9 @@ validateMechanisms<-function(d, d2_session) {
     dplyr::filter(enddate >= period_info$endDate ) %>%
     dplyr::pull(mechanism_code)
   
+  #Allow for the default mechanism
+  mechs_datim<-append("HllvX50cXC0",mechs_datim)
+  
   #Allow for the dedupe mechanisms in COP20 OPU Data Packs
   if (d$info$tool == "OPU Data Pack" & d$info$cop_year == 2020 ) {
     mechs_datim <- append(c("00000","00001"),mechs_datim)
