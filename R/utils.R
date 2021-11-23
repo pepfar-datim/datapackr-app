3cdd3e88a7a1f8dc0ccb6df356640868ebc8e911
Jason Pickering <jason.p.pickering@gmail.com>
2021-11-23 15:16:41 +0100

    Alter timestamp format for validation tests

--- R/utils.R
+++ R/utils.R
@@ -504,7 +504,7 @@ validationSummary2 <- function(d) {
                   ou_id = d$info$operating_unit$ou_id,
                   country_name = d$info$datapack_name,
                   country_uid = paste(d$info$country_uids, sep = "", collapse = ", "),
-                  ts = strftime(Sys.time() , "%Y-%m-%dT%H:%M:%S%z"),
+                  ts = strftime(Sys.time() , "%Y-%m-%d %H:%M:%S"),
                   uuid = d$info$uuid) %>%
     dplyr::filter(count > 0)
 }
