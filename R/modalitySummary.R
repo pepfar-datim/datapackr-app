faded92fe737500e7f57dab35923b58d27772273
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-03-14 14:18:50 -0400

    More linting

--- R/modalitySummary.R
+++ R/modalitySummary.R
@@ -50,7 +50,7 @@ modalitySummaryTable <- function(d) {
     hts %<>%
       tidyr::pivot_wider(names_from = resultstatus_inclusive, values_from = value) %>%
       dplyr::mutate(yield = Positive / (Negative + Positive) * 100,
-                    modality_share = Positive / sum(Positive, na.rm=TRUE) * 100,
+                    modality_share = Positive / sum(Positive, na.rm = TRUE) * 100,
                     Total = Positive + Negative) %>%
       dplyr::select(hts_modality, Positive, Total, yield, modality_share)
 
