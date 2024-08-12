e3c84b1288931a35cd8815758077ea12d1197478
Jordan <Jordan>
2024-08-12 16:45:32 -0400

    initial push for functionalizing comparison

--- server.R
+++ server.R
@@ -941,6 +941,7 @@ shinyServer(function(input, output, session) {
       }
 
       if (input$downloadType == "comparison") {
+        # sendEventToS3(d, "COMPARISON_DOWNLOAD") future upgrade
         waiter_show(html = waiting_screen_comparison, color = "rgba(128, 128, 128, .8)")
         #Create a new workbook
         flog.info(
@@ -949,72 +950,9 @@ shinyServer(function(input, output, session) {
           name = "datapack"
         )
 
-        comparison_table <-
-          d$memo$comparison %>%
-          dplyr::rename(data_type = "Data Type") %>%
-          dplyr::filter(data_type %in% c("Current", "Diff", "Proposed")) %>%
-          dplyr::filter(Identical == "FALSE") %>%
-          dplyr::select(!(Identical))
 
-        if (NROW(comparison_table) == 0L || is.null(comparison_table)) {
-          return(NULL)
-        }
+        wb <- downloadComparison(d)
 
-        wb <- openxlsx2::wb_workbook() %>%
-          openxlsx2::wb_add_worksheet("Comparison data") %>%
-          openxlsx2::wb_add_data(x = comparison_table)
-
-        df <- openxlsx2::wb_data(wb)
-
-        params <- list("colGrandTotals" = FALSE,
-                       "rowGrandTotals" = FALSE)
-
-        wb <- wb %>%
-          openxlsx2::wb_add_worksheet(sheet = "By SNU1") %>%
-          openxlsx2::wb_add_pivot_table(
-            df,
-            sheet = "By SNU1",
-            dims = "A3",
-            rows = c("Indicator", "snu1"),
-            cols = "data_type",
-            data = "value",
-            params = params)
-
-        wb <- wb %>%
-          openxlsx2::wb_add_worksheet(sheet = "By Partner") %>%
-          openxlsx2::wb_add_pivot_table(
-            df,
-            sheet = "By Partner",
-            dims = "A3",
-            rows = c("Indicator", "Partner"),
-            cols = "data_type",
-            data = "value",
-            params = params
-          )
-
-        wb <- wb %>%
-          openxlsx2::wb_add_worksheet(sheet = "By Agency") %>%
-          openxlsx2::wb_add_pivot_table(
-            df,
-            sheet = "By Agency",
-            dims = "A3",
-            rows = c("Indicator", "Agency"),
-            cols = "data_type",
-            data = "value",
-            params = params
-          )
-
-        wb <- wb %>%
-          openxlsx2::wb_add_worksheet(sheet = "By Mechanism") %>%
-          openxlsx2::wb_add_pivot_table(
-            df,
-            sheet = "By Mechanism",
-            dims = "A3",
-            rows = c("Indicator", "Mechanism"),
-            cols = "data_type",
-            data = "value",
-            params = params
-          )
 
         openxlsx2::wb_save(wb, file = file)
         waiter_hide()
